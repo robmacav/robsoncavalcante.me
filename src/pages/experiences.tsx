788c30a5b4a60661defa6364b84abd56f191c21b
Robson M. Cavalcante <robmacav@gmail.com>
2023-10-20 19:11:59 -0400

    refactor(src): list experience and about section via JSON

--- src/pages/experiences.tsx
+++ src/pages/experiences.tsx
@@ -1,3 +1,5 @@
+import experiences from '../../data/experiences.json';
+
 function Experiences() {
     return (
         <main className="container mt-5">
@@ -7,43 +9,33 @@ function Experiences() {
 
             <h3 className="my-4">Experiences</h3>
 
-            <div className="row mb-5">
-                <div className="col-12 col-sm-3 mb-2 mb-sm-0">
-                    <small className="text-secondary">2021 - Present</small>
-                </div>
-                <div className="col-12 col-sm-9">
-                    <h5 className="mb-0 fw-bold">SEFIN RO</h5>
-                    <span className="text-secondary">Full Stack Developer</span>
+            {experiences.map((item, index) => {
+                return (
+                    <div className="row mb-5" key={index}>
+                        <div className="col-12 col-sm-3 mb-2 mb-sm-0">
+                            <small className="text-secondary">2021 - Present</small>
+                        </div>
+                        <div className="col-12 col-sm-9">
+                            <h5 className="mb-0 fw-bold">{item.company}</h5>
+                            <span className="text-secondary">{item.title}</span>
 
-                    <p>I work as a Full Stack Developer in the systems implementation team that favors the control of collection, taxation and inspection processes within the state of Rondônia.</p>
+                            {item.description.map((p, index) => {
+                                return (
+                                    <p key={index}>{p}</p>
+                                )
+                            })}
 
-                    <div>
-                        <div className="badge bg-primary rounded-pill p-2 me-1 mb-1">RubyOnRails</div>
-                        <div className="badge bg-primary rounded-pill p-2 me-1 mb-1">JavaScript</div>
-                        <div className="badge bg-primary rounded-pill p-2 me-1 mb-1">Oracle Database</div>
-                        <div className="badge bg-primary rounded-pill p-2 me-1 mb-1">AWS</div>
-                        <div className="badge bg-primary rounded-pill p-2 me-1 mb-1">Wordpress</div>
+                            <div>
+                                {item.tags.map((tag, index) => {
+                                    return (
+                                        <div className="badge bg-primary rounded-pill p-2 me-1 mb-1" key={index}>{tag}</div>
+                                    )
+                                })}
+                            </div>
+                        </div>
                     </div>
-                </div>
-            </div>
-            <div className="row mb-5">
-                <div className="col-12 col-sm-3 mb-1 mb-sm-0">
-                    <small className="text-secondary">2021 - 2022</small>
-                </div>
-                <div className="col-12 col-sm-9">
-                    <h5 className="mb-0 fw-bold">Rocha Filho e Nogueira Advogados</h5>
-                    <span className="text-secondary">IT Internship</span>
-
-                    <p>I played a comprehensive role, which highlights the creation and implementation of a knowledge repository on the Intranet, using BookStack in a virtual machine, computer maintenance and improvement planning.</p>
-
-                    <div>
-                        <div className="badge bg-primary rounded-pill p-2 me-1 mb-1">Linux</div>
-                        <div className="badge bg-primary rounded-pill p-2 me-1 mb-1">HyperV</div>
-                        <div className="badge bg-primary rounded-pill p-2 me-1 mb-1">BookStack</div>
-                        <div className="badge bg-primary rounded-pill p-2 me-1 mb-1"></div>
-                    </div>
-                </div>
-            </div>
+                )
+            })}
         </main>
     )
 }
